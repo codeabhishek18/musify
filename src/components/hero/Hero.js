59211ba1a8edc9fcbfbad1ae9b18d2d6a67fb540
Abhishek Magadum <codeabhishek18@gmail.com>
2024-04-30 19:41:44 +0530

    added song cards

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,17 @@
+import '../Styles.css';
+import hero from '../../assets/hero-image.png';
+
+const Hero = () =>
+{
+    return(
+        <div className="hero-div">
+            <div className="hero-content">
+                <h1>100 Thousand Songs, ad-free</h1>
+                <h1>Over thousands podcast episodes</h1>
+            </div>
+            <img src={hero} alt="heroimage"/>
+        </div>
+    )
+}
+export default Hero;
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import Caroussel from "../components/caroussel/Caroussel"
 import FAQ from "../components/faq/FAQ"
-import Herosection from "../components/hero/Herosection"
+import Hero from "../components/hero/Hero"
 import Navbar from "../components/navbar/Navbar"
 
 const Home = () =>
@@ -8,7 +8,7 @@ const Home = () =>
     return(
         <div>
             <Navbar/>
-            <Herosection/>
+            <Hero/>
             {/* <Caroussel/>
             <FAQ/> */}
         </div>
